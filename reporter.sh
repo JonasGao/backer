dacae4059f32184e35704c499e64ce0eb14744e5
Jonas Gao <gaopinsong@live.cn>
2023-11-09 13:05:46 +0800

    feat: Download report.txt and rename.

--- reporter.sh
+++ reporter.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+[ -f report.txt ] && mv report.txt latest_report.txt
 while read -r line
 do
   gh api "repos/$line" > repo.txt
   gh release view --repo "$line" --json name,publishedAt > rele.txt
   gh api "repos/$line/commits" > lcom.txt
+  # Parse data into one row, html to report.html, txt to report.txt
   python ./parser.py "$line"
 done < ./repos
+# Send report.html
+# report.txt will upload to artifacts
 python ./mail.py
-rm report.txt
