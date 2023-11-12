5241f226836b4f10c26e401ef37e8ddac874f615
Jonas Gao <gaopinsong@live.cn>
2023-11-12 12:21:59 +0800

    fix: down_latest.sh

--- down_latest.sh
+++ down_latest.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 id=$(gh run list --repo jonasgao/backer --workflow "Reporter reporting" --limit 1 --json databaseId -q ".[0].databaseId")
-gh run download --repo jonasgao/backer "$id"
+gh run download --repo jonasgao/backer "${id//[$'\t\n\r']}"
 mv "report.txt/report.txt" "./latest_report.txt"
 rm -rf "report.txt"
-ll "latest_report.txt"
-echo "Has download latest_report.txt successfully"
+ls -al "latest_report.txt" && echo "Has download latest_report.txt successfully"
