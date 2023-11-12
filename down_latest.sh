cfac1c0b3815f5e6bc7f80276c08b2593238f14e
Jonas Gao <gaopinsong@live.cn>
2023-11-12 12:35:56 +0800

    fix: down_latest.sh

--- down_latest.sh
+++ down_latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-id=$(gh run list --repo jonasgao/backer --workflow "Reporter reporting" --limit 1 --json databaseId -q ".[0].databaseId")
+id=$(gh run list --repo jonasgao/backer --workflow "Reporter reporting" --status success --limit 1 --json databaseId -q ".[0].databaseId")
 echo "Will download artifact by $id"
 if gh run download --repo jonasgao/backer "${id//[$'\t\n\r']}"; then
   mv "report.txt/report.txt" "./latest_report.txt"
