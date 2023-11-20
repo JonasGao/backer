9477cf57c8434f2a3d4b35d219a6a6113e103cb9
Jonas Gao <gaopinsong@live.cn>
2023-11-20 16:53:54 +0800

    feat: Remove force push.

--- sync.sh
+++ sync.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
-while IFS="," read -r owner repo branch
+while IFS="," read -r owner repo branch my_repo
 do
   git clone --branch "$branch" "https://github.com/${owner}/${repo}.git" "$repo"
   pushd "$repo"
-  git remote add backup "https://${RK}@github.com/JonasGao/${repo}.git"
-  git push backup HEAD:main --force
+  git remote add backup "https://${RK}@github.com/JonasGao/${my_repo}.git"
+  git push backup HEAD:main
   popd
 done < ./sync.txt
