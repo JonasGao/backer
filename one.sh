1b4329b0ca567aaaa9cfa3427389eb40d6a2f193
Jonas Gao <gaopinsong@live.cn>
2024-03-18 11:40:59 +0800

    feat: Split sync and report.

--- one.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-./sync.sh
-echo "Finish sync."
-python ./reporter.py
-echo "Finish report."
--- sync/sync.sh
+++ sync/sync.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -e
+BASE_DIR=$(dirname "$0")
+SYNC_REPOS="$BASE_DIR/sync.txt"
+echo "Using repo list: $SYNC_REPOS"
 while IFS="," read -r owner repo branch my_repo
 do
   git clone --branch "$branch" "https://github.com/${owner}/${repo}.git" "$repo"
@@ -7,4 +10,4 @@ do
   git remote add backup "https://${RK}@github.com/JonasGao/${my_repo}.git"
   git push backup HEAD:main
   popd
-done < ./sync.txt
+done < "$SYNC_REPOS"
