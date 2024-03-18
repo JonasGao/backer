2cec2c08af096eb8ad00fe9818fe34999c1f4b2d
Jonas Gao <gaopinsong@live.cn>
2024-03-18 12:45:15 +0800

    Update sync.sh

--- sync/sync.sh
+++ sync/sync.sh
@@ -5,9 +5,10 @@ SYNC_REPOS="$BASE_DIR/sync.txt"
 echo "Using repo list: $SYNC_REPOS"
 while IFS="," read -r owner repo branch my_repo
 do
+  printf "\e[33m== Sync repo '%s' branch '%s' ==\e[0m\n" "${owner}/${repo}" "${branch}"
   git clone --branch "$branch" "https://github.com/${owner}/${repo}.git" "$repo"
-  pushd "$repo"
+  pushd "$repo" || exit 1
   git remote add backup "https://${RK}@github.com/JonasGao/${my_repo}.git"
   git push backup HEAD:main
-  popd
+  popd || exit 2
 done < "$SYNC_REPOS"
