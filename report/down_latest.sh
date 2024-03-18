a059f36fb2cb22d56a50b7a5ae1148ef959aa33f
Jonas Gao <gaopinsong@live.cn>
2024-03-18 22:39:32 +0800

    Update down_latest.sh

--- report/down_latest.sh
+++ report/down_latest.sh
@@ -9,7 +9,13 @@ echo "Will download artifact by $ID"
 echo "First cleanup, remove ${LATEST_RECORDS_NAME}"
 rm -vf "./${LATEST_RECORDS_NAME}"
 if gh run download --repo jonasgao/backer "${ID}"; then
-  mv "${ARTIFACT_NAME}/${RECORDS_NAME}" "./${LATEST_RECORDS_NAME}"
-  rm -vrf "${ARTIFACT_NAME}"
-  ls -al "${RECORDS_NAME}" && echo "Has download '${RECORDS_NAME}' successfully"
+  echo "Download successfully"
+  if mv "${ARTIFACT_NAME}/${RECORDS_NAME}" "./${LATEST_RECORDS_NAME}"; then
+    rm -vrf "${ARTIFACT_NAME}"
+    ls -al "${RECORDS_NAME}" && echo "Has download '${RECORDS_NAME}' successfully"
+  else
+    echo "Failure move '${ARTIFACT_NAME}/${RECORDS_NAME}', maybe its not exists"
+  fi
+else
+  echo "Failure download..."
 fi
