1cd8220dce4867603c0a0c2e18c8568ed6febe96
Jonas Gao <gaopinsong@live.cn>
2024-04-09 16:24:19 +0800

    Fix sync.

--- sync/sync.sh
+++ sync/sync.sh
@@ -3,12 +3,16 @@ set -e
 BASE_DIR=$(dirname "$0")
 SYNC_REPOS="$BASE_DIR/sync.txt"
 echo "Using repo list: $SYNC_REPOS"
-while IFS="," read -r owner repo branch my_repo
+while IFS="," read -r owner repo branch my_repo force
 do
   printf "\e[33m== Sync repo '%s' branch '%s' ==\e[0m\n" "${owner}/${repo}" "${branch}"
   git clone --branch "$branch" "https://github.com/${owner}/${repo}.git" "$repo"
   pushd "$repo" || exit 1
   git remote add backup "https://${RK}@github.com/JonasGao/${my_repo}.git"
-  git push backup HEAD:main
+  if [ "$force" = "force" ]; then
+    git push backup HEAD:main --force
+  else
+    git push backup HEAD:main
+  fi
   popd || exit 2
 done < "$SYNC_REPOS"
